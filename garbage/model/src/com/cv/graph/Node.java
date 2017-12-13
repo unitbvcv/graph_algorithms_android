ebc6709c927ad984b0a0f7e01ba1c4d21aef6690
Vlad Vrabie <vladvrabie97@gmail.com>
2017-12-13 12:23:02 +0200

    implemented Node class; unclear wether we need to override equals function

--- garbage/model/src/com/cv/graph/Node.java
+++ garbage/model/src/com/cv/graph/Node.java
@@ -1,4 +1,24 @@
 package com.cv.graph;
 
-public class Node {
+public class Node 
+{
+	
+	private String m_id;
+	
+	public Node(String nodeID)
+	{
+		m_id = new String(nodeID);
+	}
+
+	public String getID() 
+	{
+		return m_id;
+	}
+	
+//	public boolean equals(Object other)
+//	{
+//		if (other instanceof Node)
+//			return m_id.contentEquals(((Node)other).getID());
+//		return false;
+//	}
 }
