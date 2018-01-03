b458a6f5868a17686d320209f41fe34c974ca528
Cosmin Polifronie <cosmin.polifronie@gmail.com>
2018-01-03 23:22:02 +0200

    finish navigation menu

--- android-app/app/src/main/java/com/unitbv/cv/aggrafuri/GraphView.java
+++ android-app/app/src/main/java/com/unitbv/cv/aggrafuri/GraphView.java
@@ -2,22 +2,17 @@ package com.unitbv.cv.aggrafuri;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
 public class GraphView extends View {
-    public enum GraphViewType {
-        UNDIRECTED,
-        DIRECTED,
-        UNDIRECTED_WEIGHTED,
-        DIRECTED_WEIGHTED
-    }
-
-    private GraphViewType graphType;
+    private GraphViewType graphType = GraphViewType.NOT_SET;
 
     private Paint paint = new Paint();
+
     private float clickPositionX = -1;
     private float clickPositionY = -1;
 
@@ -26,13 +21,21 @@ public class GraphView extends View {
     private final int NODE_COLOR_G = 0;
     private final int NODE_COLOR_B = 0;
 
+    private boolean canvasNeedsClearing = false;
 
-    public GraphView(GraphViewType graphType, Context context, AttributeSet attrs) {
+    public enum GraphViewType {
+        NOT_SET,
+        UNDIRECTED,
+        DIRECTED,
+        UNDIRECTED_WEIGHTED,
+        DIRECTED_WEIGHTED
+    }
+
+    public GraphView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        this.graphType = graphType;
-
         paint.setARGB(NODE_COLOR_A, NODE_COLOR_R, NODE_COLOR_G, NODE_COLOR_B);
+
         setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -45,9 +48,26 @@ public class GraphView extends View {
         });
     }
 
+    public GraphViewType getGraphType() {
+        return graphType;
+    }
+
+    public void setGraphType(GraphViewType graphType) {
+        this.graphType = graphType;
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        //viewController.viewTouched(canvas, clickPositionX, clickPositionY);
+
+        if(canvasNeedsClearing == true) {
+            canvas.drawColor(Color.WHITE);
+            canvasNeedsClearing = false;
+        }
+    }
+
+    public void clear() {
+        canvasNeedsClearing = true;
+        invalidate();
     }
 }
--- android-app/app/src/main/java/com/unitbv/cv/aggrafuri/MainActivity.java
+++ android-app/app/src/main/java/com/unitbv/cv/aggrafuri/MainActivity.java
@@ -1,6 +1,7 @@
 package com.unitbv.cv.aggrafuri;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.util.AttributeSet;
@@ -24,6 +25,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // Set the default selection as Undirected Graph
+        navigationView.getMenu().findItem(R.id.nav_graphview_undirected).setChecked(true);
+        setNavigationButtonsState(R.id.nav_graphview_undirected);
     }
 
     @Override
@@ -36,11 +41,120 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
+    private void setNavigationButtonsState(int viewID) {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        Menu navigationMenu = navigationView.getMenu();
+        GraphView graphView = (GraphView) findViewById(R.id.graphView);
+
+        switch(viewID)
+        {
+            case R.id.nav_graphview_undirected:
+                graphView.clear();
+                graphView.setGraphType(GraphView.GraphViewType.UNDIRECTED);
+
+                navigationMenu.findItem(R.id.nav_algorithm_1).setEnabled(true);
+                navigationMenu.findItem(R.id.nav_algorithm_2).setEnabled(true);
+                navigationMenu.findItem(R.id.nav_algorithm_3).setEnabled(true);
+                navigationMenu.findItem(R.id.nav_algorithm_4).setEnabled(false);
+                navigationMenu.findItem(R.id.nav_algorithm_5).setEnabled(false);
+                navigationMenu.findItem(R.id.nav_algorithm_6).setEnabled(false);
+                navigationMenu.findItem(R.id.nav_algorithm_7).setEnabled(false);
+                navigationMenu.findItem(R.id.nav_algorithm_8).setEnabled(false);
+                navigationMenu.findItem(R.id.nav_algorithm_9).setEnabled(false);
+                navigationMenu.findItem(R.id.nav_algorithm_10).setEnabled(false);
+                break;
+
+            case R.id.nav_graphview_directed:
+                graphView.clear();
+                graphView.setGraphType(GraphView.GraphViewType.DIRECTED);
+
+                navigationMenu.findItem(R.id.nav_algorithm_1).setEnabled(true);
+                navigationMenu.findItem(R.id.nav_algorithm_2).setEnabled(true);
+                navigationMenu.findItem(R.id.nav_algorithm_3).setEnabled(true);
+                navigationMenu.findItem(R.id.nav_algorithm_4).setEnabled(false);
+                navigationMenu.findItem(R.id.nav_algorithm_5).setEnabled(false);
+                navigationMenu.findItem(R.id.nav_algorithm_6).setEnabled(false);
+                navigationMenu.findItem(R.id.nav_algorithm_7).setEnabled(false);
+                navigationMenu.findItem(R.id.nav_algorithm_8).setEnabled(false);
+                navigationMenu.findItem(R.id.nav_algorithm_9).setEnabled(false);
+                navigationMenu.findItem(R.id.nav_algorithm_10).setEnabled(true);
+                break;
+
+            case R.id.nav_graphview_undirected_weighted:
+                graphView.clear();
+                graphView.setGraphType(GraphView.GraphViewType.UNDIRECTED_WEIGHTED);
+
+                navigationMenu.findItem(R.id.nav_algorithm_1).setEnabled(true);
+                navigationMenu.findItem(R.id.nav_algorithm_2).setEnabled(true);
+                navigationMenu.findItem(R.id.nav_algorithm_3).setEnabled(true);
+                navigationMenu.findItem(R.id.nav_algorithm_4).setEnabled(true);
+                navigationMenu.findItem(R.id.nav_algorithm_5).setEnabled(true);
+                navigationMenu.findItem(R.id.nav_algorithm_6).setEnabled(true);
+                navigationMenu.findItem(R.id.nav_algorithm_7).setEnabled(false);
+                navigationMenu.findItem(R.id.nav_algorithm_8).setEnabled(false);
+                navigationMenu.findItem(R.id.nav_algorithm_9).setEnabled(false);
+                navigationMenu.findItem(R.id.nav_algorithm_10).setEnabled(false);
+                break;
+
+            case R.id.nav_graphview_directed_weighted:
+                graphView.clear();
+                graphView.setGraphType(GraphView.GraphViewType.DIRECTED_WEIGHTED);
+
+                navigationMenu.findItem(R.id.nav_algorithm_1).setEnabled(true);
+                navigationMenu.findItem(R.id.nav_algorithm_2).setEnabled(true);
+                navigationMenu.findItem(R.id.nav_algorithm_3).setEnabled(true);
+                navigationMenu.findItem(R.id.nav_algorithm_4).setEnabled(false);
+                navigationMenu.findItem(R.id.nav_algorithm_5).setEnabled(false);
+                navigationMenu.findItem(R.id.nav_algorithm_6).setEnabled(false);
+                navigationMenu.findItem(R.id.nav_algorithm_7).setEnabled(true);
+                navigationMenu.findItem(R.id.nav_algorithm_8).setEnabled(true);
+                navigationMenu.findItem(R.id.nav_algorithm_9).setEnabled(true);
+                navigationMenu.findItem(R.id.nav_algorithm_10).setEnabled(false);
+                break;
+
+            case R.id.nav_graphview_reset:
+                graphView.clear();
+                break;
+
+            case R.id.nav_algorithm_1:
+                break;
+
+            case R.id.nav_algorithm_2:
+                break;
+
+            case R.id.nav_algorithm_3:
+                break;
+
+            case R.id.nav_algorithm_4:
+                break;
+
+            case R.id.nav_algorithm_5:
+                break;
+
+            case R.id.nav_algorithm_6:
+                break;
+
+            case R.id.nav_algorithm_7:
+                break;
+
+            case R.id.nav_algorithm_8:
+                break;
+
+            case R.id.nav_algorithm_9:
+                break;
+
+            case R.id.nav_algorithm_10:
+                break;
+
+        }
+    }
+
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+
+        setNavigationButtonsState(id);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
