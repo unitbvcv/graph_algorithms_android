94792d248f192ce2a2eccfea075965544aadbf51
Vlad Vrabie <vladvra@yahoo.com>
2018-01-04 22:20:20 +0200

    first try of drawing arrows

--- android-app/app/src/main/java/com/unitbv/cv/aggrafuri/ui/GraphView.java
+++ android-app/app/src/main/java/com/unitbv/cv/aggrafuri/ui/GraphView.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.support.v4.math.MathUtils;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.Random;
 
 public class GraphView extends View {
     private GraphViewType graphType = GraphViewType.NOT_SET;
@@ -72,11 +75,61 @@ public class GraphView extends View {
             canvasNeedsClearing = false;
         }
 
-        if(clickPositionX >= 0 && clickPositionY >= 0) {
-            canvas.drawCircle(clickPositionX, clickPositionY, NODE_CIRCLE_RADIUS, paint);
-            clickPositionX = -1;
-            clickPositionY = -1;
+//        if(clickPositionX >= 0 && clickPositionY >= 0) {
+//            canvas.drawCircle(clickPositionX, clickPositionY, NODE_CIRCLE_RADIUS, paint);
+//            clickPositionX = -1;
+//            clickPositionY = -1;
+//        }
+
+        int k = 30;
+
+        Random generator = new Random();
+        int x1 = generator.nextInt(500);
+        int y1 = generator.nextInt(500);
+        int x2 = generator.nextInt(500);
+        int y2 = generator.nextInt(500);
+
+        double alpha, alpha1, alpha2;
+
+
+        // Caz orizontal
+        if (y1 == y2)
+        {
+            if (x2 > x1)
+                alpha = 0.0f;
+            else
+                alpha = Math.PI;
         }
+        else
+        {
+            if (x1 == x2) // caz vertical
+            {
+                if (y2 > y1)
+                    alpha = Math.PI / 2;
+                else
+                    alpha = 3 * Math.PI / 2;
+            }
+            else // caz oblic
+                alpha = Math.atan(  ((double)(y2 - y1)) / (x2 - x1) );
+        }
+
+        alpha1 = alpha + Math.PI / 6;
+        alpha2 = alpha - Math.PI / 6;
+
+        int x3 = (int) (x2 - k * Math.cos(alpha1));
+        int y3 = (int) (y2 - k * Math.sin(alpha1));
+
+        int x4 = (int) (x2 - k * Math.cos(alpha2));
+        int y4 = (int) (y2 - k * Math.sin(alpha2));
+
+        // First line from (x1, y1) to (x2, y2)
+        canvas.drawLine(x1, y1, x2, y2, paint);
+
+        // Second line from (x2, y2) to (x3, y3)
+        canvas.drawLine(x2, y2, x3, y3, paint);
+
+        // Third line from (x2, y2) to (x4, y4)
+        canvas.drawLine(x2, y2, x4, y4, paint);
     }
 
     public void clear() {
