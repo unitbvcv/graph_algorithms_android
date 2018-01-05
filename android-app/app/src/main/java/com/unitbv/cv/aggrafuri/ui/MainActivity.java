465a269b261d7bc5cfa5b2177874162a262f09de
Cosmin Polifronie <cosmin.polifronie@gmail.com>
2018-01-05 21:44:54 +0200

    add a test window for input (first algorithm)

--- android-app/app/src/main/java/com/unitbv/cv/aggrafuri/ui/MainActivity.java
+++ android-app/app/src/main/java/com/unitbv/cv/aggrafuri/ui/MainActivity.java
@@ -1,12 +1,16 @@
 package com.unitbv.cv.aggrafuri.ui;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import com.unitbv.cv.aggrafuri.R;
 
@@ -112,6 +116,30 @@ public class MainActivity extends AppCompatActivity
                 break;
 
             case R.id.nav_algorithm_1:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Title");
+
+                // Set up the input
+                final EditText input = new EditText(this);
+                // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(input);
+
+                // Set up the buttons
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        input.getText().toString();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
                 break;
 
             case R.id.nav_algorithm_2:
