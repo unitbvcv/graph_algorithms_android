8ebc632484d3c92575142f6b09d17891cc0e63c9
Vlad Vrabie <vladvra@yahoo.com>
2018-01-05 18:53:24 +0200

    implementing drawing Edge between 2 Nodes

--- android-app/app/src/main/java/com/unitbv/cv/aggrafuri/ui/GraphView.java
+++ android-app/app/src/main/java/com/unitbv/cv/aggrafuri/ui/GraphView.java
@@ -28,6 +28,8 @@ public class GraphView extends View {
     private final int NODE_COLOR_B = 0;
     private final float NODE_CIRCLE_RADIUS = 50;
 
+    private final float NODE_STROKE_WIDTH = 3.0f;
+
     private final int BACKGROUND_COLOR_R = 255;
     private final int BACKGROUND_COLOR_G = 255;
     private final int BACKGROUND_COLOR_B = 255;
@@ -48,13 +50,18 @@ public class GraphView extends View {
         super(context, attrs);
 
         paint.setARGB(NODE_COLOR_A, NODE_COLOR_R, NODE_COLOR_G, NODE_COLOR_B);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(NODE_STROKE_WIDTH);
 
         setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
-                clickPositionX = motionEvent.getX();
-                clickPositionY = motionEvent.getY();
-                invalidate();
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP)
+                {
+                    clickPositionX = motionEvent.getX();
+                    clickPositionY = motionEvent.getY();
+                    invalidate();
+                }
                 return true;
             }
         });
@@ -77,20 +84,96 @@ public class GraphView extends View {
             canvasNeedsClearing = false;
         }
 
-        canvas.drawCircle(500, 500,30, paint);
-
         if (clickPositionX != -1 && clickPositionY != -1) {
-            canvas.drawLine(500, 500, clickPositionX, clickPositionY, paint);
-            canvas.drawCircle(clickPositionX, clickPositionY, 30, paint);
+            Random gen = new Random();
 
-            double angle = Math.angleOfLineToOx(500, 500, clickPositionX, clickPositionY);
+            int x1 = gen.nextInt(getWidth());
+            int y1 = gen.nextInt(getHeight());
+            int x2 = gen.nextInt(getWidth());
+            int y2 = gen.nextInt(getHeight());
 
-            Log.d("INFO", "TOUCH POINT: " + clickPositionX + " " + clickPositionY);
-            Log.d("INFO", "ALPHA: " + angle + " (" + java.lang.Math.toDegrees(angle) + "°)");
+            paint.setColor(Color.RED);
+            canvas.drawArc(x1 - NODE_CIRCLE_RADIUS, y1 - NODE_CIRCLE_RADIUS,
+                    x1 + NODE_CIRCLE_RADIUS, y1 + NODE_CIRCLE_RADIUS,
+                    0.0f, 360.0f, false, paint);
+            paint.setColor(Color.BLACK);
+
+            canvas.drawArc(x2 - NODE_CIRCLE_RADIUS, y2 - NODE_CIRCLE_RADIUS,
+                    x2 + NODE_CIRCLE_RADIUS, y2 + NODE_CIRCLE_RADIUS,
+                    0.0f, 360.0f, false, paint);
+
+            double angleOfLine = Math.angleOfLineToOx(x1, y1, x2, y2);
+            int tangent_x_A, tangent_y_A, tangent_x_B, tangent_y_B;
+            double alpha_A;
+
+            if (x1 <= x2)
+            {
+                if (y1 <= y2) // SE
+                {
+                    alpha_A = angleOfLine;
+                    tangent_x_A = (int) (x1 + NODE_CIRCLE_RADIUS * java.lang.Math.sin(alpha_A) / java.lang.Math.tan(angleOfLine));
+                    tangent_y_A = (int) (y1 + NODE_CIRCLE_RADIUS * java.lang.Math.sin(alpha_A));
+
+                    tangent_x_B = (int) (x2 - NODE_CIRCLE_RADIUS * java.lang.Math.cos(alpha_A));
+                    tangent_y_B = (int) (y2 - NODE_CIRCLE_RADIUS * java.lang.Math.cos(alpha_A) * java.lang.Math.tan(angleOfLine));
+                }
+                else // NE
+                {
+                    alpha_A = 2 * java.lang.Math.PI - angleOfLine;
+                    tangent_x_A = (int) (x1 + NODE_CIRCLE_RADIUS * java.lang.Math.cos(alpha_A) );
+                    tangent_y_A = (int) (y1 + NODE_CIRCLE_RADIUS * java.lang.Math.cos(alpha_A) * java.lang.Math.tan(angleOfLine));
+
+                    tangent_x_B = (int) (x2 + NODE_CIRCLE_RADIUS * java.lang.Math.sin(alpha_A) / java.lang.Math.tan(angleOfLine));
+                    tangent_y_B = (int) (y2 + NODE_CIRCLE_RADIUS * java.lang.Math.sin(alpha_A));
+                }
+            }
+            else
+            {
+                if (y1 <= y2) // SV
+                {
+                    alpha_A = java.lang.Math.PI - angleOfLine;
+                    tangent_x_A = (int) (x1 + NODE_CIRCLE_RADIUS * java.lang.Math.sin(alpha_A) / java.lang.Math.tan(angleOfLine));
+                    tangent_y_A = (int) (y1 + NODE_CIRCLE_RADIUS * java.lang.Math.sin(alpha_A));
+
+                    tangent_x_B = (int) (x2 + NODE_CIRCLE_RADIUS * java.lang.Math.cos(alpha_A) );
+                    tangent_y_B = (int) (y2 + NODE_CIRCLE_RADIUS * java.lang.Math.cos(alpha_A) * java.lang.Math.tan(angleOfLine));
+                }
+                else // NV
+                {
+                    alpha_A = angleOfLine - java.lang.Math.PI;
+                    tangent_x_A = (int) (x1 - NODE_CIRCLE_RADIUS * java.lang.Math.cos(alpha_A));
+                    tangent_y_A = (int) (y1 - NODE_CIRCLE_RADIUS * java.lang.Math.cos(alpha_A) * java.lang.Math.tan(angleOfLine));
+
+                    tangent_x_B = (int) (x2 + NODE_CIRCLE_RADIUS * java.lang.Math.sin(alpha_A) / java.lang.Math.tan(angleOfLine));
+                    tangent_y_B = (int) (y2 + NODE_CIRCLE_RADIUS * java.lang.Math.sin(alpha_A));
+                }
+            }
+
+            Log.d("INFO", "x1: " + x1 + " y1: " + y1 + " x2: " + x2 + " y2: " + y2);
+            Log.d("INFO", "angleOfLine: " + java.lang.Math.toDegrees(angleOfLine) +
+                    " alphaA: " + java.lang.Math.toDegrees(alpha_A));
+            Log.d("INFO", "tangent_x_A: " + tangent_x_A + " tangent_y_A: " + tangent_y_A +
+            " tangent_x_B: " + tangent_x_B + " tangent_y_B: " + tangent_y_B);
+
+            canvas.drawLine(tangent_x_A, tangent_y_A, tangent_x_B, tangent_y_B, paint);
 
             clickPositionX = -1;
             clickPositionY = -1;
         }
+//        canvas.drawCircle(500, 500,30, paint);
+//
+//        if (clickPositionX != -1 && clickPositionY != -1) {
+//            canvas.drawLine(500, 500, clickPositionX, clickPositionY, paint);
+//            canvas.drawCircle(clickPositionX, clickPositionY, 30, paint);
+//
+//            double angle = Math.angleOfLineToOx(500, 500, clickPositionX, clickPositionY);
+//
+//            Log.d("INFO", "TOUCH POINT: " + clickPositionX + " " + clickPositionY);
+//            Log.d("INFO", "ALPHA: " + angle + " (" + java.lang.Math.toDegrees(angle) + "°)");
+//
+//            clickPositionX = -1;
+//            clickPositionY = -1;
+//        }
     }
 
     public void clear() {
--- android-app/app/src/main/java/com/unitbv/cv/aggrafuri/ui/GraphViewController.java
+++ android-app/app/src/main/java/com/unitbv/cv/aggrafuri/ui/GraphViewController.java
@@ -1,7 +1,17 @@
 package com.unitbv.cv.aggrafuri.ui;
 
+import android.view.MotionEvent;
+import android.view.View;
+
 public class GraphViewController {
     public GraphViewController() {
 
     }
+
+//    public boolean onTouch(View view, MotionEvent motionEvent) {
+//        clickPositionX = motionEvent.getX();
+//        clickPositionY = motionEvent.getY();
+//        invalidate();
+//        return true;
+//    }
 }
