b1be31cc695df50f76dab59a3d606d36fcec8f8b
Vlad Vrabie <vladvra@yahoo.com>
2018-01-06 19:31:29 +0200

    added model class <=> wrapper over AbstractGraph

--- android-app/app/src/main/java/com/unitbv/cv/aggrafuri/graph/GraphModel.java
+++ android-app/app/src/main/java/com/unitbv/cv/aggrafuri/graph/GraphModel.java
@@ -1,7 +1,35 @@
 package com.unitbv.cv.aggrafuri.graph;
 
 public class GraphModel {
+
     AbstractGraph graph;
+    GraphType type = GraphType.NOT_SET;
+
+    public GraphModel(GraphType type)
+    {
+        setType(type);
+    }
+
+    private void initGraph() {
+        switch (this.type)
+        {
+            case NOT_SET:
+                graph = null;
+                break;
+            case UNDIRECTED:
+                graph = new UndirectedGraph();
+                break;
+            case DIRECTED:
+                graph = new DirectedGraph();
+                break;
+            case UNDIRECTED_WEIGHTED:
+                graph = new UndirectedWeightedGraph();
+                break;
+            case DIRECTED_WEIGHTED:
+                graph = new DirectedWeightedGraph();
+                break;
+        }
+    }
 
     public AbstractGraph getGraph() {
         return graph;
@@ -11,4 +39,12 @@ public class GraphModel {
         this.graph = graph;
     }
 
+    public GraphType getType() {
+        return type;
+    }
+
+    public void setType(GraphType type) {
+        this.type = type;
+        initGraph();
+    }
 }
